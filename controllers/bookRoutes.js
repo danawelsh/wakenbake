df2821165e8db8d5dc44f232cf47ae3f1c670ed6
danawelsh <111257868+danawelsh@users.noreply.github.com>
2023-03-23 14:22:05 -0400

    added code to get info for the recipe book

---
+++ controllers/bookRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Recipe, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const recipeData = await Recipe.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const recipes = recipeData.map((project) => Recipe.get({ plain: true }));
+
+    res.render('recipe', { 
+      recipes, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
