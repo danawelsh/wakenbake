ceb71e555631d6b1a3f88df1f4446f129a2c67e2
danawelsh <111257868+danawelsh@users.noreply.github.com>
2023-03-24 15:34:00 -0400

    renamed the Public folder to public, moved the api/recipeapi to the public folder since it is a third party front end api, deleted the recipeRoutes.js file because it wasn't needed

--- models/index.js
+++ models/index.js
@@ -1,13 +1,2 @@
 const User = require('./User');
-const Post = require('./recipe');
-
-User.hasMany(Recipe, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
-
-Post.belongsTo(User, {
-  foreignKey: 'user_id'
-});
-
-module.exports = { User, Recipe };
+module.exports = { User };
