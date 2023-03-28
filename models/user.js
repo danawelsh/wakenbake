9fe5eebb1cd8327594e425e59c92e75e79bf8839
chloegithub <miss_jihyun@hotmail.com>
2023-03-28 19:20:41 -0400

    fixing logout

--- models/user.js
+++ models/user.js
@@ -11,6 +11,25 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
   },
   {
     sequelize,
