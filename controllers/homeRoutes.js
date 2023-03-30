876249e5cbc5f6689bcecf1c123e96f283c9e1ab
chloegithub <miss_jihyun@hotmail.com>
2023-03-30 17:54:55 -0400

    search recipe with images

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-const { description, recipe } = require('../models');
+const { description, recipe, User } = require('../models');
+const { upsert } = require('../models/recipe');
 //const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
@@ -25,26 +26,24 @@ router.get('/search', async (req, res) => {
   res.render('search')
 })
 
-// router.get('/profile', async (req, res) => {
-//     try {
-//       const userData = await User.findByPk(req.session.user_id, {
-//         attributes: { exclude: ['password'] },
-//       });
+router.get('/profile', async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.session.user_id);
   
-//       const user = userData.get({ plain: true });
-  
-//       res.render('profile', {
-//         ...user,
-//         logged_in: true
-//       });
-//     } catch (err) {
-//       res.status(500).json(err);
-//     }
-//   });
+       const user = await userData.get({plain: true});
+  console.log(user)
+      res.render('profile', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
   
   router.get('/login', (req, res) => {
     if (req.session.logged_in) {
-      res.redirect('/profile');
+      res.redirect('/');
       return;
     }
   
